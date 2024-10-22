1ec036db93c83439028e0bb6d10c0f19c02d5a0e
Paulo Rogério <paulorogeriojp@gmail.com>
2024-10-21 23:33:20 -0300

    projeto base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
+
+class App extends Component {
+   state ={
+      posts:[
+        {
+          id: 1,
+          title: 'O titulo 1',
+          body: 'O corpo 1'
+        },
+        {
+          id: 2,
+          title: 'O titulo 2',
+          body: 'O corpo 2'
+        },
+        {
+          id: 3,
+          title: 'O titulo 3',
+          body: 'O corpo 3np'
+        },
+      ]
+   }
+
+
+render(){
+  const { posts } = this.state;
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h1 >{post.title}</h1>
+          <p >{post.body}</p>
+        </div>
+      ))}
     </div>
   );
-}
+ }
+} 
 
 export default App;
